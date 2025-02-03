4eb8b4cdc4cbc9df8109a7d60a6eb222afa19662
Owen Thomas <owen.thomas@gotinder.com>
2025-02-02 20:52:55 -0800

    Add Scene1

---
+++ MultiStripLED/Scene1.cpp
@@ -0,0 +1,151 @@
+// Scene1.cpp
+#include "Scene1.h"
+#include "LED.h"
+#include "LEDSection.h"
+
+const int PIXEL_COUNT = 300;    // Total number of LEDs
+const int PIXEL_PIN = 6;       // Pin connected to the NeoPixel strip
+
+Scene1::Scene1(Adafruit_NeoPixel* strip) :
+  sections {
+    LEDSection(strip, 0, 49),
+    LEDSection(strip, 50, 99),
+    LEDSection(strip, 100, 149),
+    LEDSection(strip, 150, 199),
+    LEDSection(strip, 200, 249),
+    LEDSection(strip, 250, 299)
+
+    // section6,
+    // section8,
+    // LEDSection(&strip, 70, 79)
+  },
+  fullStrip(strip, 0, 299)
+{
+}
+
+void Scene1::setup() {
+
+    strip->begin();   // Initialize section 1
+    // strip.clear();   // Initialize section 3
+
+    strip->setBrightness(3);
+    strip->show();    // Turn off all LEDs in section 3
+}
+
+void Scene1::loop() {
+  eraseAll(0);
+return;
+  // for (int i = 0; i < NUM_STRIPS - 4; i++) {
+  //   rainbowWipe(strip, 0 + PIXELS_PER_STRIP * i, PIXELS_PER_STRIP + (PIXELS_PER_STRIP * i), 10, true);
+  // }
+  for (int i = 0; i < sizeof(sections) / sizeof(sections[0]); i++) {
+    rainbowWipe(sections[i], 1, true);
+  }
+  // rainbowWipe(section1, 0, sizeof(pixels2) / sizeof(pixels2[0]), 100, true);
+  // rainbowWipe(section2, 0, sizeof(pixels2) / sizeof(pixels2[0]), 100, true);
+  // strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip->show();
+  // strips[1].show();
+  // Show the changes
+    // strip.show();
+
+  delay(200); // Wait for 1 second
+  // for (int i = 0; i < sizeof(sections) / sizeof(sections[0]); i++) {
+  //   // rainbowWipe(sections[i], 1, true);
+  //   rainbow(sections[i], 10, 3, false);
+  // }
+  rainbow(fullStrip, 5, 3, false);
+  eraseAll(1);
+}
+
+
+void Scene1::rainbowWipe(Adafruit_NeoPixel* strip, int start, int end, int wait, bool forwards) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+    if(forwards) {
+      for(int i=start; i<end; i++) { // For each pixel in strip...
+        int pixelHue = 0 + ((end - i - start) * 65536L / (end - start));
+        strip->setPixelColor(i, strip->gamma32(strip->ColorHSV(pixelHue)));
+        strip->show(); // Update strip with new contents
+        delay(wait);
+      }
+    } else {
+        for(int i=end; i>start; i--) { // For each pixel in strip...
+        int pixelHue = 0 + ((end - i - start) * 65536L / (end - start));
+        strip->setPixelColor(i, strip->gamma32(strip->ColorHSV(pixelHue)));
+        strip->show(); // Update strip with new contents
+        delay(wait);
+      }
+    }
+    // strip.show(); // Update strip with new contents
+    // delay(wait);  // Pause for a moment
+}
+void Scene1::rainbowWipe(LEDSection section, int wait, bool forwards) {
+  rainbowWipe(section, 0, section.numPixels, wait, forwards);
+}
+
+void Scene1::rainbowWipe(LEDSection section, int start, int end, int wait, bool forwards) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+    if(forwards) {
+      for(int i=start; i<end; i++) { // For each pixel in strip...
+        int pixelHue = 0 + ((end - i - start) * 65536L / (end - start));
+        section[i]->setPixelColor(strip->gamma32(strip->ColorHSV(pixelHue)));
+        strip->show(); // Update strip with new contents
+        delay(wait);
+      }
+    } else {
+        for(int i=end; i>start; i--) { // For each pixel in strip...
+        int pixelHue = 0 + ((end - i - start) * 65536L / (end - start));
+        section[i]->setPixelColor(strip->gamma32(strip->ColorHSV(pixelHue)));
+        strip->show(); // Update strip with new contents
+        delay(wait);
+      }
+    }
+    // strip.show(); // Update strip with new contents
+    // delay(wait);  // Pause for a moment
+}
+
+void Scene1::rainbow(LEDSection section, int wait, int cycles, bool forwards) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < cycles*65536; firstPixelHue += 256) {
+    if(forwards) {
+      for(int i=0; i<section.numPixels; i++) { // For each pixel in strip...
+        // Offset pixel hue by an amount to make one full revolution of the
+        // color wheel (range of 65536) along the length of the strip
+        // (strip.numPixels() steps):
+        int pixelHue = firstPixelHue + (i * 65536L / section.numPixels);
+        // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+        // optionally add saturation and value (brightness) (each 0 to 255).
+        // Here we're using just the single-argument hue variant. The result
+        // is passed through strip.gamma32() to provide 'truer' colors
+        // before assigning to each pixel:
+        section[i]->setPixelColor(strip->gamma32(strip->ColorHSV(pixelHue)));
+      }
+    } else {
+      for(int i=section.numPixels - 1; i>=0; i--) {
+        int pixelHue = firstPixelHue + ((section.numPixels - i) * 65536L / section.numPixels);
+        section[i]->setPixelColor(strip->gamma32(strip->ColorHSV(pixelHue)));
+      }
+    }
+    strip->show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
+void Scene1::eraseAll(int wait) {
+  for(int i = 0; i < PIXEL_COUNT; i++) {
+    strip->setPixelColor(i, strip->Color(0, 0, 0));
+    if(wait > 0) {
+      strip->show();
+      delay(wait);
+    }
+  }
+}
---
+++ MultiStripLED/Scene1.h
@@ -0,0 +1,25 @@
+// Scene1.h
+#ifndef Scene1_H
+#define Scene1_H
+
+#include <Adafruit_NeoPixel.h>
+#include "LEDSection.h"
+
+class Scene1 {
+public:
+  Scene1(Adafruit_NeoPixel* strip);
+  void setup();
+  void loop();
+
+private:
+  Adafruit_NeoPixel* strip;
+  LEDSection sections[6];
+  LEDSection fullStrip;
+  void rainbowWipe(Adafruit_NeoPixel* strip, int start, int end, int wait, bool forwards);
+  void rainbowWipe(LEDSection section, int wait, bool forwards);
+  void rainbowWipe(LEDSection section, int start, int end, int wait, bool forwards);
+  void rainbow(LEDSection section, int wait, int cycles, bool forwards);
+  void eraseAll(int wait);
+};
+
+#endif
